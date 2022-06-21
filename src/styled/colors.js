2136fc4a662dd6ddbb2d248bc8fc1282f5ca1ecb
Gabriel Garcia <gabrielgsdev@gmail.com>
2022-06-20 19:03:16 -0500

    Add theme

---
+++ src/styled/colors.js
@@ -0,0 +1,14 @@
+const colors = {
+  primary: '#1797FF',
+  gray1: '#ECECEC',
+  screenBackground: '#FCFCFC',
+  white: '#FFFFFF',
+  defaultTab: '#606060',
+  borderTab: '#d6d6d6',
+  text1: '#343434',
+  text2: '#6B6B6B',
+  textDate: '#767676',
+  borderCard: '#979797',
+};
+
+export default colors;
