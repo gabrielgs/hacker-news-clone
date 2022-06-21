aa6cea4d892645fd78d777f40a67cffe2ed0ac79
Gabriel Garcia <gabrielgsdev@gmail.com>
2022-06-21 05:30:21 -0500

    Add dropdown select

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 import Tabs from './Tabs';
+import DropdownSelect from './DropdownSelect';
 
-const Wrapper = styled.main``;
+const Wrapper = styled.main`
+  padding: 0 32px;
+`;
 
 const Main = () => (
   <Wrapper>
     <Tabs />
+    <DropdownSelect />
   </Wrapper>
 );
 
